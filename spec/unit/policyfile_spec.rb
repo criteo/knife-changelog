6e400c48750240848224df41911d1a770999c66e
Baptiste Courtois <b.courtois@criteo.com>
2021-07-07 12:15:31 +0200

    policyfile: add tests on git_ref for tags prefixed by cookbook-name
    Change-Id: I30458270e893625709c3da6942f5e8e009abce5b

--- spec/unit/policyfile_spec.rb
+++ spec/unit/policyfile_spec.rb
@@ -238,11 +238,16 @@ RSpec.describe PolicyChangelog do
 
     context 'when tag invalid and able to correct' do
       it 'returns correct git tag' do
-        allow(repo).to receive(:checkout).with(/v1.0/).and_raise(::Git::GitExecuteError)
         allow(repo).to receive(:checkout).with('1.0.0').and_raise(::Git::GitExecuteError)
-        allow(repo).to receive(:checkout).with('1.0').and_return(true)
 
-        expect(changelog.git_ref('1.0.0', repo)).to eq('1.0')
+        tags = %w[v1.0.0 1.0 v1.0 cookbook_name-1.0.0 cookbook_name-1.0 cookbook_name-v1.0.0 cookbook_name-v1.0]
+        tags.each do |valid_result|
+          allow(repo).to receive(:checkout).with(valid_result).and_return(true)
+          tags.reject { |v| v == valid_result }.each do |invalid_result|
+            allow(repo).to receive(:checkout).with(invalid_result).and_raise(::Git::GitExecuteError)
+          end
+          expect(changelog.git_ref('1.0.0', repo, 'cookbook_name')).to eq valid_result
+        end
       end
     end
 
